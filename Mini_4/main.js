856aa5848ae06f48d30b9cdc421d360dbd26bc78
Bobby Mulligan <robertpmulligan@gmail.com>
2018-09-17 19:41:13 -0500

    Add files via upload
    Initial dropping in files. All files work and do not require any fixes

---
+++ Mini_4/main.js
@@ -0,0 +1,32 @@
+﻿window.onload = function() {
+	var canvas = document.getElementById("canvas"),
+		context = canvas.getContext("2d"),
+		width = canvas.width = window.innerWidth,
+		height = canvas.height = window.innerHeight,
+		radius = 10,
+		rect = {
+			x: width / 2 - 200,
+			y: height / 2 - 150,
+			width: 400,
+			height: 300
+		};
+
+	function clamp(value, min, max) {
+		return Math.min(Math.max(value, min), max);
+	}
+
+	document.body.addEventListener("mousemove", function(event) {
+		var x = clamp(event.clientX, rect.x + radius, rect.x + rect.width - radius),
+			y = clamp(event.clientY, rect.y + radius, rect.y + rect.height - radius);
+
+		context.clearRect(0, 0, width, height);
+		context.fillStyle = "#cccccc";
+		context.fillRect(rect.x, rect.y, rect.width, rect.height);
+
+		context.fillStyle = "#000000";
+		context.beginPath();
+		context.arc(x, y, radius, 0, Math.PI * 2, false);
+		context.fill();
+	});
+
+};
