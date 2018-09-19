941827f3b5d31fcd38fe5328bf9da6f9504f8ea4
Bobby Mulligan <robertpmulligan@gmail.com>
2018-09-18 19:18:31 -0500

    Add files via upload

---
+++ Mini_6/main2.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+	var canvas = document.getElementById("canvas"), 
+		context = canvas.getContext("2d), 
+		width = window.width = 
+		height = window; 
+
+	function randomRange(min, max) {
+		return min + Math.random() * (max - min); 
+	}
+	
+	function randomInt(min, max) {
+		return Math.floor(min + Math.random() * (max - min + 1)); 
+	}
+
+
+}; 
