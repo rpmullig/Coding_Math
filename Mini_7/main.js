cbcaeb6bd43f67b8c07a42079d3c66683e432dbd
Bobby Mulligan <robertpmulligan@gmail.com>
2018-10-22 17:49:31 -0500

    Add files via upload

---
+++ Mini_7/main.js
@@ -0,0 +1,22 @@
+﻿window.onload = function() {
+	var canvas = document.getElementById("canvas"),
+		context = canvas.getContext("2d"),
+		width = canvas.width = window.innerWidth,
+		height = canvas.height = window.innerHeight;
+
+	var angle = utils.degreesToRads(132); 
+
+	context.translate(width / 2, height  / 2); 
+	context.rotate(angle); 
+
+	context.beginPath(); 
+	context.arc(0, 0, 20, 0, Math.PI * 2, 0); 
+	context.fill();
+
+	context.lineWidth = 8; 
+	context.beginPath(); 	
+	context.moveTo(0, 0); 
+	context.lineTo(150,0); 
+	context.stroke();
+
+};
